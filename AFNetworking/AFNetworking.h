8f0633047e95a2b731e6d9345285557c0a3ccfa1
Robert Böhnke <robb@robb.is>
2015-04-10 17:07:57 +0200

    Fix Headers

--- AFNetworking/AFNetworking.h
+++ AFNetworking/AFNetworking.h
@@ -35,10 +35,8 @@
     #import "AFHTTPRequestOperation.h"
     #import "AFHTTPRequestOperationManager.h"
 
-#if TARGET_OS_IPHONE
-    #import "AFNetworkingiOS.h"
-#else
-    #import "AFNetworkingOSX"
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+    #import "UIKit+AFNetworking.h"
 #endif
 
 #if ( ( defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090) || \
